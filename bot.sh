9c246f9f162c15cf8fc56eb1a951466e1aa557dd
songyoungoh <112578065+songyoungoh@users.noreply.github.com>
2022-09-30 21:07:17 +0000

    added chatbot bash file

---
+++ bot.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 2 ]
+then
+        echo "Error"
+        exit 0
+fi
+
+# Telegram Bot Information
+ID="5551671825"
+API_TOKEN="5675712941:AAHxNBva2DvqKzS2FOIsBv7LMzfupoBTnaY"
+URL="https://api.telegram.org/bot${API_TOKEN}/sendMessage"
+
+# Date
+DATE="$(date "+%Y-%m-%d")"
+
+# Messages
+TEXT="${DATE} $1 $2"
+
+curl -s -d "chat_id=${ID}&text=${TEXT}" ${URL} > /dev/null
