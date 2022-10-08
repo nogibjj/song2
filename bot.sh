e5f608a1d35504afa7e0b7141ef40b1fde9e2447
songyoungoh <112578065+songyoungoh@users.noreply.github.com>
2022-10-08 14:11:06 +0000

    modified bash file

--- bot.sh
+++ bot.sh
@@ -6,11 +6,6 @@ then
         exit 0
 fi
 
-# Telegram Bot Information
-ID="5551671825"
-API_TOKEN="5675712941:AAHxNBva2DvqKzS2FOIsBv7LMzfupoBTnaY"
-URL="https://api.telegram.org/bot${API_TOKEN}/sendMessage"
-
 # Date
 DATE="$(date "+%Y-%m-%d")"
 
@@ -18,3 +13,8 @@ DATE="$(date "+%Y-%m-%d")"
 TEXT="${DATE} [$1] $2"
 
 curl -s -d "chat_id=${ID}&text=${TEXT}" ${URL} > /dev/null
+
+# Telegram Bot Information
+ID="5551671825"
+API_TOKEN="5675712941:AAHxNBva2DvqKzS2FOIsBv7LMzfupoBTnaY"
+URL="https://api.telegram.org/bot${API_TOKEN}/sendMessage"
