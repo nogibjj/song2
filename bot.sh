4729f4b0e752888ea1dc2d8605aae1ec3f68c2b5
songyoungoh <112578065+songyoungoh@users.noreply.github.com>
2022-10-09 02:25:37 +0000

    modified bash command line tool

--- bot.sh
+++ bot.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# Telegram Bot Information
-ID="5551671825"
-API_TOKEN="5741127105:AAGFW-7oTqftW2pZXjmUkAWc4yV8i-Zr7sc"
-URL="https://api.telegram.org/bot${API_TOKEN}/sendMessage"
-
-
-
-
-
-
 if [ $# -ne 2 ]
 then
         echo "Error"
@@ -21,5 +11,10 @@ DATE="$(date "+%Y-%m-%d")"
 
 # Messages
 TEXT="${DATE} [$1] $2"
+
+# Telegram Bot Information
+ID="..."
+API_TOKEN="..."
+URL="https://api.telegram.org/bot${API_TOKEN}/sendMessage"
 
 curl -s -d "chat_id=${ID}&text=${TEXT}" ${URL} > /dev/null
